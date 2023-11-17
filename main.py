37ed3a330c578823ff32d8e8041a58b6a89777d4
Dio Lemos <pedro.devsc@outlook.com>
2023-11-17 18:29:09 -0300

    add github link to the home page

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+from static.keys import connection_str
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+
 
 app = Flask(__name__)
+
+mongo_uri = connection_str
+client = MongoClient(mongo_uri)
+db = client.get_database("ulims")
+searches_collection = db.searches
 
 
 @app.route("/")
@@ -12,10 +20,17 @@ def respostas():
     return render_template("teste.html")
 
 
-@app.route("/cep")
+@app.route("/cep", methods=["GET", "POST"])
 def cep():
-    return render_template("cep.html")
+    search_history = searches_collection.find()
+    if request.method == "POST":
+        cep_value = request.form.get("cep")
+        save_search(cep_value)
+    return render_template("cep.html", search_history=search_history)
 
+
+def save_search(cep):
+    searches_collection.insert_one({"cep": cep})
 if __name__ == "__main__":
     app.run(debug=True)
     
